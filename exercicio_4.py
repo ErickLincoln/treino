32dc03e069acd309fe080ab4312032b377b416f0
Erick Lincoln <ericklincoln@live.com>
2022-09-05 19:41:02 -0300

    finalizações de exercicio

--- exercicio_4.py
+++ exercicio_4.py
@@ -16,7 +16,12 @@ idadeMedia = (sara + mark + fatima)/3
 print("A idade média de Sara, Mark e Fátima é de", int(idadeMedia) ,"anos.")
 
 print("\n######################\n")
+print("___(C)____________________________________________")
 #(c) O número de vezes que 73 cabe em 403.
 numsubtraido = 403
 subtraidor = 73
-print(int(numsubtraido/subtraidor))
+quociente = int(numsubtraido/subtraidor)
+print("O número de vezes que", subtraidor ,"está em", numsubtraido ,"é" ,quociente , '!')
+#nota: Para saber quantas vezes um número cabe dentro de outro, peguemos o número 
+# e bata dividir pelo número que queremos saber quantas vezes tal número cabe nele.
+#sendo assim: a título de estudos criarei uma versão mais aprofundada(exercicio_4_variação).
