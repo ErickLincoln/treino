497f366f733000b1f00b85c4cee3768114da10df
Erick Lincoln <ericklincoln@live.com>
2022-09-05 00:50:27 -0300

    pratica 03 estruturação de laço iterativo finalizada

---
+++ exercidio_3.py
@@ -0,0 +1,6 @@
+# a soma dos cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100)
+# usando laço iterativo
+numeroNatural = 0
+while (numeroNatural < 101):
+    print(numeroNatural)
+    numeroNatural += 1
