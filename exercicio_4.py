d4b5270fe632f55415205f1ee688f616afe8bad6
Erick Lincoln <ericklincoln@live.com>
2022-09-05 01:33:02 -0300

    pratica 04 e exercicio de variações afim de aumento de conhecimento

---
+++ exercicio_4.py
@@ -0,0 +1,22 @@
+print("\n######################\n")
+# (a) A soma dos 5 primeiros inteiros positivos.
+print("___(A)___")
+positivos = 1
+while (positivos <= 4):
+    print(positivos, "+ 1 =" , positivos+1)
+    positivos += 1
+
+print("\n######################\n")
+#(b) A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
+print("___(B)____________________________________________")
+sara = 23
+mark = 19
+fatima = 31
+idadeMedia = (sara + mark + fatima)/3
+print("A idade média de Sara, Mark e Fátima é de", int(idadeMedia) ,"anos.")
+
+print("\n######################\n")
+#(c) O número de vezes que 73 cabe em 403.
+numsubtraido = 403
+subtraidor = 73
+print(int(numsubtraido/subtraidor))
---
+++ exercicio_4_variacao.py
@@ -0,0 +1,9 @@
+print("\n######################\n")
+#Variação_(4.c) O número de vezes que um número cabe em outro.
+divisor = float(input("Quantas vezes o número: "))
+dividendo = float(input("está em? "))
+quociente = float(dividendo/divisor)
+print("O número de vezes que", divisor ,"está em", dividendo,"é" ,quociente)
+# Quando fazemos uma divisão, estamos distribuindo uma quantidade 
+# por um certo número de partes iguais. No entanto, é possível interpretar isto como:
+# "Quantas vezes o este número está em naquele?"
