339e13dd655b6c8a26987227e0d8ded86705ae42
Erick Lincoln <ericklincoln@live.com>
2022-09-05 22:09:03 -0300

    Execução e finalização da prática 05

---
+++ exercicio_5.py
@@ -0,0 +1,12 @@
+print("\n######################\n")
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca'] #lista criada
+print('Da lista: ',palavras)
+
+palavras.sort() #A função 'sort'  ordena a lista em ordem alfabética
+print('A primeira palavra:' , palavras[0]) #utilizei '[0]' para retornar o primeiro indice
+palavras.sort(reverse = True) #A função 'sort' 
+#com a opção 'reverse = True' ordena a lista em ordem alfabética reversa
+print('A última palavra:' , palavras[0]) #utilizei '[0]' para retornar o primeiro indice
+print('Segundo a ordem de dicionário.')
+print("\n######################\n")
+
